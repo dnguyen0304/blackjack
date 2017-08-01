236db46678d33cfac8f6dab778cfde294aa0bd9a
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-01 16:11:39 -0400

    Added CardShuffleStrategy.

---
+++ src/main/java/com/github/dnguyen0304/blackjack/CardShuffleStrategy.java
@@ -0,0 +1,15 @@
+package com.github.dnguyen0304.blackjack;
+
+/**
+ * This strategy provides the algorithm for shuffling a single deck.
+ *
+ * Decks are shuffled in-place.
+ *
+ * @author Duy Nguyen
+ * @see DeckShuffleStrategy
+ */
+public interface CardShuffleStrategy {
+
+	public void shuffle(Deck deck);
+
+}
