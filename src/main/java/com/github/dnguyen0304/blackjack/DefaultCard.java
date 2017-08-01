9d124cad63dd1f59d2a30f501e6defbf80e2a901
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-01 17:37:29 -0400

    Changed indentation from tabs to spaces.

--- src/main/java/com/github/dnguyen0304/blackjack/DefaultCard.java
+++ src/main/java/com/github/dnguyen0304/blackjack/DefaultCard.java
@@ -2,20 +2,22 @@ package com.github.dnguyen0304.blackjack;
 
 public class DefaultCard implements Card {
 
-	private final Rank rank;
-	private final Suit suit;
+    private final Rank rank;
+    private final Suit suit;
 
-	public DefaultCard(Rank rank, Suit suit) {
-		this.rank = rank;
-		this.suit = suit;
-	}
-	
-	public Rank getRank() {
-		return rank;
-	}
+    public DefaultCard(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
 
-	public Suit getSuit() {
-		return suit;
-	}
+    @Override
+    public Rank getRank() {
+        return rank;
+    }
+
+    @Override
+    public Suit getSuit() {
+        return suit;
+    }
 
 }
