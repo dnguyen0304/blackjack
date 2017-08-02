576ca1d109e5a4ee3a2c88f4ea51706e39f184b7
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-02 14:40:17 -0400

    Added Player.

---
+++ src/main/java/com/github/dnguyen0304/blackjack/Player.java
@@ -0,0 +1,11 @@
+package com.github.dnguyen0304.blackjack;
+
+public interface Player {
+
+    public String getName();
+
+    public int getChipTotal();
+
+    public void setChipTotal(int chipTotal);
+
+}
