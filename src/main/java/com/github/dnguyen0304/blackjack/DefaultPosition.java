677927b61005a8acdb8f0aa3a0bcd6a18d4d066f
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-02 13:46:44 -0400

    Added DefaultPosition.

---
+++ src/main/java/com/github/dnguyen0304/blackjack/DefaultPosition.java
@@ -0,0 +1,33 @@
+package com.github.dnguyen0304.blackjack;
+
+public class DefaultPosition implements Position {
+
+    private Player player;
+    private int bet;
+
+    @Override
+    public Boolean isOpen() {
+        return this.player != null;
+    }
+
+    @Override
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    @Override
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public int getBet() {
+        return this.bet;
+    }
+
+    @Override
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+}
