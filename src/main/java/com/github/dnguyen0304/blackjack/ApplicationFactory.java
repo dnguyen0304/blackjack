e36053e71485d5cec9c28052835c0ff6dac3208f
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-02 15:47:09 -0400

    Added ApplicationFactory.

---
+++ src/main/java/com/github/dnguyen0304/blackjack/ApplicationFactory.java
@@ -0,0 +1,8 @@
+package com.github.dnguyen0304.blackjack;
+
+public interface ApplicationFactory {
+
+    // How do you define a generic method signature for returning any object?
+    public Application create();
+
+}
