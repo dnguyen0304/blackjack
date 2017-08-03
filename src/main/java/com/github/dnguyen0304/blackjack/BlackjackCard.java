fa8a6dab37ff34a4866648632f2c32f0e22a726a
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-03 17:49:48 -0400

    Added value to BlackjackCard.

--- src/main/java/com/github/dnguyen0304/blackjack/BlackjackCard.java
+++ src/main/java/com/github/dnguyen0304/blackjack/BlackjackCard.java
@@ -1,6 +1,29 @@
 package com.github.dnguyen0304.blackjack;
 
+import java.util.HashMap;
+import java.util.Map;
+
+// This ValuedCard could implement the Card and Valuable interfaces.
 public class BlackjackCard {
+
+    // This is called a "static initialization block".
+    private static final Map<Rank, Integer> ranksToValues;
+    static {
+        ranksToValues = new HashMap<Rank, Integer>();
+        ranksToValues.put(Rank.ACE, 1);
+        ranksToValues.put(Rank.TWO, 2);
+        ranksToValues.put(Rank.THREE, 3);
+        ranksToValues.put(Rank.FOUR, 4);
+        ranksToValues.put(Rank.FIVE, 5);
+        ranksToValues.put(Rank.SIX, 6);
+        ranksToValues.put(Rank.SEVEN, 7);
+        ranksToValues.put(Rank.EIGHT, 8);
+        ranksToValues.put(Rank.NINE, 9);
+        ranksToValues.put(Rank.TEN, 10);
+        ranksToValues.put(Rank.JACK, 10);
+        ranksToValues.put(Rank.QUEEN, 10);
+        ranksToValues.put(Rank.KING, 10);
+    }
 
     private final Rank rank;
     private final Suit suit;
@@ -16,6 +39,10 @@ public class BlackjackCard {
 
     public Suit getSuit() {
         return suit;
+    }
+
+    public int value() {
+        return BlackjackCard.ranksToValues.get(this.rank);
     }
 
 }
