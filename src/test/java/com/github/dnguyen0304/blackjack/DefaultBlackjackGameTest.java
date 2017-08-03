d75eab92347d4cf149ab9715b1353bb4e727cc47
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-03 10:18:26 -0400

    Changed "table" naming convention to "game".

--- src/test/java/com/github/dnguyen0304/blackjack/DefaultBlackjackGameTest.java
+++ src/test/java/com/github/dnguyen0304/blackjack/DefaultBlackjackGameTest.java
@@ -9,7 +9,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class DefaultBlackjackTableTest {
+public class DefaultBlackjackGameTest {
 
     @Rule
     public final ExpectedException exception = ExpectedException.none();
