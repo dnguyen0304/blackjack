d520eba4457e76a251c8ce8573cd8d817eb74a53
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-04 15:06:41 -0400

    Added DealingPlayer.

---
+++ src/main/java/com/github/dnguyen0304/blackjack/DealingPlayer.java
@@ -0,0 +1,33 @@
+package com.github.dnguyen0304.blackjack;
+
+public class DealingPlayer implements Player, Drawable {
+
+    private Player player;
+    private Drawable deck;
+
+    public DealingPlayer(Player player, Drawable deck) {
+        this.player = player;
+        this.deck = deck;
+    }
+
+    @Override
+    public String getName() {
+        return this.player.getName();
+    }
+
+    @Override
+    public int getChipTotal() {
+        return this.player.getChipTotal();
+    }
+
+    @Override
+    public void setChipTotal(int chipTotal) {
+        this.player.setChipTotal(chipTotal);
+    }
+
+    @Override
+    public BlackjackCard draw() {
+        return this.deck.draw();
+    }
+
+}
