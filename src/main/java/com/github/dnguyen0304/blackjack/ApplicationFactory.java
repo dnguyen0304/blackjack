94b96379c4db9c11e1f67717151ea3e1f3d81733
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-04 14:55:14 -0400

    Changed create method stub for ApplicationFactory.

--- src/main/java/com/github/dnguyen0304/blackjack/ApplicationFactory.java
+++ src/main/java/com/github/dnguyen0304/blackjack/ApplicationFactory.java
@@ -2,6 +2,9 @@ package com.github.dnguyen0304.blackjack;
 
 public class ApplicationFactory {
 
+    private static final String DEALER_NAME = "Dealer";
+    private static final int DECK_COUNT = 8;
+
     private final String[] args;
 
     public ApplicationFactory(String[] args) {
@@ -10,7 +13,25 @@ public class ApplicationFactory {
 
     // How do you define a generic method signature for returning any object?
     public Application create() {
-        Application application = new Application();
+        DealablePositionFactory positionFactory = new DealablePositionFactory();
+        BlackjackGameBuilder builder = new BlackjackGameBuilder(positionFactory);
+
+        // Create the players.
+        for (String name : this.args) {
+            Player player = new Player(name);
+            builder.withPlayer(player);
+        }
+
+        // Create the dealer.
+        Player dealer = new Player(ApplicationFactory.DEALER_NAME);
+        builder.withPlayer(dealer);
+
+        // Create the game.
+        BlackjackGame game = builder.build();
+
+        // Create the application.
+        Application application = new Application(game);
+
         return application;
     }
 
