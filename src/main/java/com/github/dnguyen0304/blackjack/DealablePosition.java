307178f255b4af10d82d4716bce5d0985c8a9bc0
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-04 12:50:04 -0400

    Changed Player setter to constructor parameter for DealablePosition.

--- src/main/java/com/github/dnguyen0304/blackjack/DealablePosition.java
+++ src/main/java/com/github/dnguyen0304/blackjack/DealablePosition.java
@@ -4,16 +4,16 @@ public class DealablePosition {
 
     private Player player;
 
-    public Boolean isOpen() {
-        return this.player == null;
+    public DealablePosition(Player player) {
+        this.player = player;
     }
 
     public Player getPlayer() {
         return this.player;
     }
 
-    public void setPlayer(Player player) {
-        this.player = player;
+    public Boolean isOpen() {
+        return this.player == null;
     }
 
 }
