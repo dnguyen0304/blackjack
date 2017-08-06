0a598db2bee262d8a1f10209de0fe172e98f6962
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-06 13:16:37 -0400

    Added bet to BlackjackDealer.

--- src/main/java/com/github/dnguyen0304/blackjack/BlackjackDealer.java
+++ src/main/java/com/github/dnguyen0304/blackjack/BlackjackDealer.java
@@ -2,11 +2,11 @@ package com.github.dnguyen0304.blackjack;
 
 public class BlackjackDealer implements Dealer {
 
-    private Player player;
+    private CardGamePlayer player;
     private Drawable deck;
     private CardShuffler cardShuffler;
 
-    public BlackjackDealer(Player player, Drawable deck, CardShuffler cardShuffler) {
+    public BlackjackDealer(CardGamePlayer player, Drawable deck, CardShuffler cardShuffler) {
         this.player = player;
         this.deck = deck;
         this.cardShuffler = cardShuffler;
@@ -23,8 +23,8 @@ public class BlackjackDealer implements Dealer {
     }
 
     @Override
-    public void setChipTotal(int chipTotal) {
-        this.player.setChipTotal(chipTotal);
+    public int bet() {
+        return 0;
     }
 
     @Override
@@ -35,6 +35,24 @@ public class BlackjackDealer implements Dealer {
     @Override
     public BlackjackCard draw() {
         return this.deck.draw();
+    }
+
+    @Override
+    public BlackjackHand getHand() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addToHand(BlackjackCard card) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int compareTo(CardGamePlayer o) {
+        // TODO Auto-generated method stub
+        return 0;
     }
 
 }
