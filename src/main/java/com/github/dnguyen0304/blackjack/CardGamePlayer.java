23e339d200f7281d90261ccd985da6f4063994c6
Duy Nguyen <dnguyen0304@gmail.com>
2017-08-06 12:40:12 -0400

    Added getHand to CardGamePlayer.

--- src/main/java/com/github/dnguyen0304/blackjack/CardGamePlayer.java
+++ src/main/java/com/github/dnguyen0304/blackjack/CardGamePlayer.java
@@ -2,6 +2,8 @@ package com.github.dnguyen0304.blackjack;
 
 public interface CardGamePlayer extends Player, Comparable<CardGamePlayer> {
 
+    public BlackjackHand getHand();
+
     public void addToHand(BlackjackCard card);
 
 }
